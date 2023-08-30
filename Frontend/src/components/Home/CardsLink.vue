c1ee632df09abdba482e2135ff50201a41e9a25b
HugoRomz <rosalesrafael1@hotmai.com>
2023-08-29 22:41:47 -0600

    Terminado home y creacion de Ensenanza

---
+++ Frontend/src/components/Home/CardsLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-link :to="card.route">
+        <div class="my-8 border border-gray-500  rounded shadow-lg shadow-gray-200 bg-white duration-300 hover:-translate-y-1 ">
+                <figure>
+                    <img :src="card.img" :alt="card.alt" class="rounded h-72 w-full object-cover">
+                    <figcaption class="p-4">
+                        <p class="text-lg mb-4 font-bold leading-relaxed text-gray-800 ">{{ card.title }}</p>
+                        <small class="leading-5 text-gray-500">{{ card.description }}</small>
+                    </figcaption>
+                </figure>
+            </div>
+        </router-link>
+</template>
+
+<script>
+    export default{
+        props: ['card']
+    }
+</script>
--- Frontend/src/views/EnsenanzaView.vue
+++ Frontend/src/views/EnsenanzaView.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-        
+        <HomeEnsenanza />
     </div>
 </template>
 
 <script>
 
+    import HomeEnsenanza from '../components/Ensenanza/HomeEnsenanza.vue';
+
+    export default {
+        components: {
+            HomeEnsenanza
+        }
+    }
 </script>
