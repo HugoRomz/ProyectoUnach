c52c02d291e0beb5771022ff3fa2d1e27e3bdca5
HugoRomz <rosalesrafael1@hotmai.com>
2023-08-30 02:28:37 -0600

    Agregamos tabView-Ensenanza

---
+++ Frontend/src/components/Ensenanza/TabApuntes.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full p-4 ">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+         Mi Botón3
+</button>
+    </div>
+</template>
