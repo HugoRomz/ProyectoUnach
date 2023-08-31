6f63e3bc3fc4bd2f3a7ce779e9681deaf4c6bf93
HugoRomz <rosalesrafael1@hotmai.com>
2023-08-31 01:01:55 -0600

    Eliminado bootstrap

--- Frontend/src/components/Ensenanza/TabEjercicios.vue
+++ Frontend/src/components/Ensenanza/TabEjercicios.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="w-full p-4 ">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-         Mi Botón1
+         Agregar
 </button>
     </div>
 </template>
+
+<script>
+
+</script>
