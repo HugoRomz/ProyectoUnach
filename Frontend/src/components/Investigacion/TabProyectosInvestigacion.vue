b84712b2f5a8cb236ab03ae6fd72466bbaf96406
TheAnonymous491 <theanonymous491@gmail.com>
2023-10-13 12:58:10 -0500

    Plantilla de Coordinacion de investigacion

---
+++ Frontend/src/components/Investigacion/TabProyectosInvestigacion.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="w-full p-4">
+    <div class="w-full">
+      <DataTableComponent
+        :data="proyectos"
+        :columns="columns"
+        :dtoptions="dtoptions"
+      >
+        <template #headers>
+          <th>No_Proyecto</th>
+          <th>Nombre</th>
+          <th>Fecha Inicio</th>
+          <th>Fecha Fin</th>
+          <th>Lider del Proyecto</th>
+          <th>Status</th>
+          <th>Acciones</th>
+        </template>
+      </DataTableComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import DataTableComponent from "../Plantillas/DataTableComponent.vue";
+
+export default {
+  components: {
+    DataTableComponent,
+  },
+  data() {
+    return {
+      proyectos: [],
+      columns: [
+        { data: "idProyecto" },
+        { data: "nombreProyecto" },
+        {
+          data: "fechaInicio",
+          render: function (data, type, row) {
+            if (type === "display" || type === "filter") {
+              return dayjs(data).format("YYYY-MM-DD"); // ajusta el formato como desees
+            }
+            return data;
+          },
+        },
+        {
+          data: "fechaFin",
+          render: function (data, type, row) {
+            if (type === "display" || type === "filter") {
+              return dayjs(data).format("YYYY-MM-DD"); // ajusta el formato como desees
+            }
+            return data;
+          },
+        },
+        { data: "liderProyecto" },
+        { data: "status" },
+        {
+          title: "Acciones",
+          data: null,
+          render: (data, type, row) => {
+            return `
+                        <button class="btn-editar-actividad bg-yellow-500 text-white p-2 pt-3 rounded" data-id="${data.idActTutorias}"><i class="pi pi-pencil pointer-events-none"></i></button>
+                        <button class="btn-eliminar-actividad bg-red-500 text-white  p-2 pt-3  rounded" data-id="${data.idActTutorias}"><i class="pi pi-trash pointer-events-none"></i></button>
+                        <button class="btn-detalle-actividad bg-blue-500 text-white p-2 pt-3 rounded" data-id="${data.idActTutorias}"><i class="pi pi-info-circle pointer-events-none"></i></button>
+                      `;
+          },
+        },
+      ],
+      dtoptions: {
+        dom: "Bfrtip",
+        language: {
+          search: "Buscar",
+          zeroRecords: "No hay registros para mostrar",
+          info: "Mostrando del _START_ al _END_ de _TOTAL_ registros",
+          infoFiltered: "(Filtrados de _MAX_ registros)",
+          paginate: {
+            first: "Primero",
+            previous: "Anterior",
+            next: "Siguiente",
+            last: "Ultimo",
+          },
+        },
+        buttons: [
+          {
+            tittle: "Reporte de actividades PAT",
+            extend: "excelHtml5",
+            text: "Excel",
+            className: "bg-green-500 btn btn-success border-0",
+          },
+          {
+            tittle: "Reporte de actividades PAT",
+            extend: "pdfHtml5",
+            text: '<i class="fa-regular fa-file"></i> PDF',
+            className: "bg-red-500 btn btn-danger border-0",
+            customize: function (doc) {
+              // Personalizar el documento PDF aquí
+
+              // Añadir margen superior al título
+              if (doc.content[0].text) {
+                // Verifica si hay un título
+                doc.content[0].margin = [0, 10, 0, 0]; // 50 es el margen superior
+              }
+
+              // Añadir margen superior a la tabla para moverla hacia abajo
+              if (doc.content[1].table) {
+                // Verifica si hay una tabla
+                doc.content[1].margin = [0, 10, 0, 0]; // 70 es el margen superior
+              }
+              doc["header"] = function (currentPage, pageCount, pageSize) {
+                return {
+                  image: logoSuperior,
+                  width: 550,
+                  alignment: "center",
+                  margin: [0, 28, 0, 25], // Ajusta según necesites
+                };
+              };
+
+              doc["footer"] = function (currentPage, pageCount, pageSize) {
+                return {
+                  image: logoInferior,
+                  width: 450,
+                  alignment: "center",
+                  margin: [0, 10, 0, 10], // Ajusta según necesites
+                };
+              };
+            },
+          },
+
+          {
+            tittle: "Reporte de actividades PAT",
+            extend: "print",
+            text: "Imprimir",
+            className: "bg-gray-500 btn btn-dark border-0",
+          },
+          {
+            tittle: "Reporte de actividades PAT",
+            extend: "copy",
+            text: "Copiar Texto",
+            className: "bg-slate-300 btn btn-light border-0",
+          },
+        ],
+      },
+    };
+  },
+  mounted() {
+    
+  },
+};
+</script>
---
+++ Frontend/src/views/InvestigacionView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container mx-auto">
+    <HeaderComponent
+      titulo="Coordinacion de Investigación"
+      ciclo="AGTO - DIC 2023"
+    />
+    <div class="w-full bg-white shadow-xl border rounded-lg border-gray-300 mb-7">
+      <button
+        @click="showModal = true"
+        class="bg-blue-500 text-white font-bold p-2 rounded m-4"
+      >
+        Registrar Proyecto
+      </button>
+      <TabProyectosInvestigacion ref="datatableComponent" />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "../components/HeaderModuleComponent.vue";
+import TabProyectosInvestigacion from "../components/Investigacion/TabProyectosInvestigacion.vue"
+
+export default{
+    components:{
+        HeaderComponent,
+        TabProyectosInvestigacion
+    },
+    data(){
+
+    },
+    methods:{
+        handleClose(value) {
+            this.showModal = value;
+        },
+        fetchActivities() {
+            this.$refs.datatableComponent.updateData();
+        },
+    }
+}
+
+</script>
